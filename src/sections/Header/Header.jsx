a8c7204745e1906f0de5bf30876b8a954853a859
jalilmanuel25 <jalilmanuel25@gmail.com>
2021-12-18 16:36:37 -0600

    se termina header

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { Navbar } from '../../components/Navbar/Navbar'
 import './header.css'
+import { gsap } from "gsap";
 
 export const Header = () => {
+    const boxRef = useRef();
+
+    // wait until DOM has been rendered
+    // useEffect(() => {
+    //     gsap.to(boxRef.current, { rotation: "+=360" });
+    // });
+
     return (
         <header className="bg-1">
             <div className="container">
                 <Navbar /> 
-
+                <div className="contain-header">
+                    <div className="info-header">
+                        <h1 className="tit-header">Desarrollador-Programador</h1>
+                        <h2 className="name">Jalil López</h2>
+                        <p>TSU en Tecnologías de la Información</p>
+                        <p className="btn">Ver proyectos</p>
+                    </div>
+                    <img ref={boxRef} className="img-programming" src='img/programming.svg' alt="programming" />
+                </div>
             </div>
         </header>
     )
