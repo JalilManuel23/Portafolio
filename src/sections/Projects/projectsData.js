e8ad5ca19d04efb1c0142ba12ca91743bd3c6354
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-01-04 19:24:18 -0600

    se solucionan detalles

--- src/sections/Projects/projectsData.js
+++ src/sections/Projects/projectsData.js
@@ -8,25 +8,7 @@ const projects = [
     {
         "id": 2,
         "name": "Full Wings",
-        "image": "img/projects/brighit.png",
-        "description": "Sitio web publicitario para un restaurante local, sitio de administración del mismo."
-    },
-    {
-        "id": 3,
-        "name": "Full Wings2",
-        "image": "img/projects/brighit.png",
-        "description": "Sitio web publicitario para un restaurante local, sitio de administración del mismo."
-    },
-    {
-        "id": 4,
-        "name": "Full Wings3",
-        "image": "img/projects/brighit.png",
-        "description": "Sitio web publicitario para un restaurante local, sitio de administración del mismo."
-    },
-    {
-        "id": 5,
-        "name": "Full Wings4",
-        "image": "img/projects/brighit.png",
+        "image": "img/projects/fullwings.jpg",
         "description": "Sitio web publicitario para un restaurante local, sitio de administración del mismo."
     },
 ]
