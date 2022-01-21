ae069ebf2c9e74510d22eaeaeb9f0f00d37da144
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-01-21 13:21:16 -0600

    se agreaga curso de m001 a skills

--- src/sections/Skills/coursesData.js
+++ src/sections/Skills/coursesData.js
@@ -7,12 +7,18 @@ const courses = [
     },
     {
         "id": 2,
+        "name": "Curso MongoDB Basics",
+        "image": "img/courses/m001.jfif",
+        "description": "Fundamentos de MongoDB"
+    },
+    {
+        "id": 3,
         "name": "Curso de PHP",
         "image": "img/courses/php.jpg",
         "description": "Realizado en la plataforma de Udemy"
     },
     {
-        "id": 3,
+        "id": 4,
         "name": "Curso de JQuery",
         "image": "img/courses/jquery.jpg",
         "description": "Realizado en la plataforma de Udemy"
