5b47ad8c39bab832fb7071ec6649b9099748ebf7
Jalil <jalilmanuel25@gmail.com>
2022-07-01 19:59:09 -0500

    navbar fijo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,34 @@
 import React, { useState, useEffect } from 'react'
 import './navbar.css'
-import { gsap } from "gsap";
 
 export const Navbar = () => {
     const [navClass, setNavClass] = useState( false );
 
-    const toogle = () => { 
-        setNavClass( !navClass )
+
+    const changeClass=()=>{
+        const scrollValue=document.documentElement.scrollTop;
+        if(scrollValue>100)
+        {
+            setNavClass( true );
+        }
+        else{
+            setNavClass( false );
+        }
     }
 
-    const timeline = gsap.timeline();
-
-    // wait until DOM has been rendered
-    useEffect(() => {
-        const animation = document.querySelectorAll('.navbar');
-        timeline.from( animation, { opacity: 0, x: 50, y: 40, duration: 1, stagger: 0.3 } )
-    });
+    window.addEventListener('scroll', changeClass);
 
     return (
-        <nav className="navbar">
+        <nav className={ navClass ? "navbar active" : "navbar" } >
             <div className="brand-title">
                 <span>&#8832;</span> Jalil López <span>&#8827;</span> 
             </div>
-            <a href="#" className="toggle-button" onClick={ toogle }>
+            <a href="#" className="toggle-button">
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
             </a>
-            <div className={ navClass ? "navbar-links active" : "navbar-links" }>
+            <div className="navbar-links">
                 <ul>
                     <li><a href="#">Inicio</a></li>
                     <li><a href="#aboutme">Sobre mi</a></li>
