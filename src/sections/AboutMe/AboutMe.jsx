88e30b4436d339d7e1a57871e88ab5f2f991e21a
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-07-23 21:50:55 -0500

    Avances about me

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -16,31 +16,18 @@ export const AboutMe = () => {
 
     return (
         <section id='aboutme' className="about-me bg-1">
-            <div className="about-me-content" data-aos="fade-up">
-                <Title text="Sobre mí" />
-                <div className="about-me-info container">
-                    <div className="img-name">
-                        <img src="img/jalil.png" alt="Jalil foto" className="jalil-foto" />
-                        <p>Jalil López</p>
-                        <div className="icons-img-name">
-                            <a href="mailto:jalilmanuel25@gmail.com"><FontAwesomeIcon icon={ faEnvelope } /></a>
-                            <a href="https://github.com/JalilManuel23" target="_blank"><FontAwesomeIcon icon={ faGithub } /></a>
-                        </div>
+            <div className="container">
+                <div>
+                    <h4>Sobre mi</h4>
+                    <p>Desarrollador web. TSU en TI</p>
+                    <div className='contacto-links'>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
                     </div>
-                    <div className="info-ubi">
-                        <p>
-                            Soy Jalil López, tengo 20 años
-                            estudio ingeniería de software,
-                            en la Universidad Tecnológica
-                            de Durango.
-                        </p>
-                        <div className="ubi">
-                            <FontAwesomeIcon icon={ faMapMarker } />
-                            <p>Durango, Dgo, Méx.</p>
-                        </div>
-                    </div>
-                    <img src="img/trayectoria.png" alt="tayectoria" className="trayectoria" />
                 </div>
+                <img src="img/projects/brighit.png" alt="about me" srcset="img/projects/brighit.png" />
             </div>
         </section>
     )
