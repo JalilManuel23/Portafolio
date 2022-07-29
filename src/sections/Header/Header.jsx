52a1074ed2f43b0525a15f2ae52acddb8b8d0071
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-07-28 20:11:59 -0500

    Se cambia color de header

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -15,17 +15,11 @@ export const Header = () => {
     });
 
     return (
-        <header className="header h-vh-100 bg-primary">
+        <header className="header h-vh-100">
             <Navbar />
             <div className='name d-flex flex-column justify-content-end align-items-center pt-5'>
                 <h1 className='title2'>Jalil López.</h1>
-                <TypeAnimation
-                    className='fw-bold fs-4 text-uppercase'
-                    cursor={false}
-                    sequence={['Desarrollador Web', 3000,'TSU Tecnologías de la Información', 2000]}
-                    wrapper="h1"
-                    repeat={Infinity}
-                />
+                <h4>Web Developer</h4>
             </div>
             <div className='scroll-container d-flex flex-column justify-content-end align-items-center'>
                 <p className='p-0 m-0'>SCROLL</p>
