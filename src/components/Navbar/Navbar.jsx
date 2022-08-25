1dbf61cdacf0ed488bc89b701037c4bca68f2edd
Jalil M. López <jalilmanuel25@gmail.com>
2022-08-24 19:25:42 -0500

    Cambiar opción del menú

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ export const Navbar = () => {
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#aboutme">About me</a></li>
-                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#skills">Skills</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
